8e26259a5ca12dff335e9c3a449e1462f1b9dc1c
laercioss <71737006+laercioss@users.noreply.github.com>
2025-04-20 15:26:20 +0000

    Teste

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
-    res.send('Olá, sou progamador BackEnd Jr !!!')
+    res.send('Olá, sou progamador BackEnd Jr!!!')
 })
 
 app.get('/sobre', function(req, res){
